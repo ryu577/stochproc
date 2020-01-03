69a01d1028569d2c6ac9f7ec0aebe3246b7c6670
Rohit Pandey <rohitpandey576@gmail.com>
2020-01-02 16:26:52 -0800

    Create binom_sum_experiments.py

---
+++ stochproc/hypothesis/experiments/binom_sum_experiments.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import comb
+
+
+def binom_partial_sum(n,p=.5):
+    b_sum=0
+    for j in range(int(n/2)+1):
+        b_sum+=comb(n,j)*(1+p)**j
+    return b_sum/(2+p)**n
+
+sums = np.array([binom_partial_sum(i,p=0.5) for i in range(11,501,2)])
+sums2 = (2+0.5-0.5**0.5)**np.arange(11,501,2)/(2+0.5)**np.arange(11,501,2)
+
+plt.plot(np.arange(11,501,2),sums,label="partial sum")
+plt.plot(np.arange(11,501,2),sums2,label="approx")
+plt.legend()
+plt.show()
+
+#https://www.wolframalpha.com/input/?i=sum+%28n+choose+j%29+%281%2Bp%29%5Ej%2C+j%3Dn%2F2%2B1+to+n
+#https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5839521/
