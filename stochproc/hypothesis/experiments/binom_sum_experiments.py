798944fedd141b22ffae4f6a485030431ee3515a
Rohit Pandey <ropandey@microsoft.com>
2020-01-05 19:17:00 -0800

    Small changes to binom_sum_experiments.py

--- stochproc/hypothesis/experiments/binom_sum_experiments.py
+++ stochproc/hypothesis/experiments/binom_sum_experiments.py
@@ -7,15 +7,14 @@ def binom_partial_sum(n,p=.5):
     b_sum=0
     for j in range(int(n/2)+1):
         b_sum+=comb(n,j)*(1+p)**j
-    return b_sum/(2+p)**n
+    return b_sum#/(2+p)**n
 
 def partial_sum_closed_form(n,p=0.5):
     return 1-comb(n,n/2+1)*hyp2f1(1,1-n/2, n/2+2, -p-1)*(1+p)**(n/2+1)/(2+p)**n
 
 
-sums = np.array([binom_partial_sum(i,p=0.5) for i in range(11,501,2)])
-sums2 = (2+0.5-0.5**0.5)**np.arange(11,501,2)/(2+0.5)**np.arange(11,501,2)
-
+sums = np.array([binom_partial_sum(i,p=0.1) for i in range(11,501,2)])
+sums2 = ((2+0.1)*0.9999)**np.arange(11,501,2)/(2+0.1)**np.arange(11,501,2)
 
 plt.plot(np.arange(11,501,2),sums,label="partial sum")
 plt.plot(np.arange(11,501,2),sums2,label="approx")
