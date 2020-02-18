bdfaffd7ecee13feab9b4685d7b456034238e21b
Rohit Pandey <rohitpandey576@gmail.com>
2020-02-17 16:11:03 -0800

    Modified setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
 
 setup(name='stochproc',
-      version='0.0.0',
+      version='0.0.2',
       url='https://github.com/ryu577/stochproc',
       license='MIT',
       author='Rohit Pandey',
       author_email='rohitpandey576@gmail.com',
       description='Methods to model all kinds of stochastic processes.',
-      packages=find_packages(exclude=['tests']),
+      packages=find_packages(exclude=['tests','plots']),
       long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      #long_description='Modeling all kinds of stochstic processes. Coin toss sequences, failure rates and most recently, hypothesis testing.',
       zip_safe=False)
 
